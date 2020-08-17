6eba9462b6ef62bc425046a364d613bdcf49b8dc
Leon <Liangz.org@gmail.com>
2020-08-17 15:16:01 +0800

    feat(Bin2Dec): add bin2dec function

--- 1-Beginner/Bin2Dec/Bin2Dec.py
+++ 1-Beginner/Bin2Dec/Bin2Dec.py
@@ -11,5 +11,29 @@
 -------------------------------------------------------------------------------
 """
 
+import math
 
 
+def bin2dec(binary: str) -> int:
+    length = len(binary)
+    res = 0
+    for index, bit in enumerate(binary):
+        p = length - 1 - index
+        res += int(int(bit) * math.pow(2, p))
+    return res
+
+
+def check(binary: str) -> bool:
+    for i in binary:
+        if int(i) > 1:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    while True:
+        s = input("请输入二进制字符串:")
+        if check(s):
+            print(bin2dec(s))
+        else:
+            print("Input is invalid")
